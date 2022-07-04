7be022d232bee1569d314d9bbde083e6e293be6a
krishnasajeeva <59118545+krishnasajeeva@users.noreply.github.com>
2022-07-04 17:31:16 +0530

    Add files via upload

---
+++ TW/Chsong_main.java
@@ -0,0 +1,11 @@
+package TW;
+
+public class Chsong_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		chsong song1= new chsong();
+		song1.getVerses();
+	}
+
+}
